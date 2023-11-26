bda5396b4f4456cd5907d89352f962e7e7758a76
SkelAlex <alexandre.fortier.chouinard@mail.utoronto.ca>
2023-11-25 20:23:55 -0500

    Tests tri Trudeau

--- ChapitreBilan.R
+++ ChapitreBilan.R
@@ -82,3 +82,100 @@ mean(nchar(Promises$Libellé.en), na.rm = T)
 mean(nchar(Promises$Libellé.fr), na.rm = T)
 mean(nchar(Promesses$Libellé.EN))
 mean(nchar(Promesses$Libellé.FR))
+
+# Tri Trudeau
+AllPromises <- openxlsx::read.xlsx(paste0(
+  "../polimetre-dev/_SharedFolder_polimetre-fonctionnement/6. Polimètre",
+  " Fédéral (Trudeau-44)/",
+  "3. Divers/Sélection des promesses étroites et importantes/TriFinal.xlsm")
+)[1:577,]
+AllPromisesRevised <- openxlsx::read.xlsx(paste0(
+  "../polimetre-dev/_SharedFolder_polimetre-fonctionnement/6. Polimètre",
+  " Fédéral (Trudeau-44)/PolimètreTrudeau.xlsx"), 2) |>
+  dplyr::filter(Mandat == 3)
+matched_rows <- matrix(NA, nrow = nrow(AllPromisesRevised),
+                       ncol = nrow(AllPromises))
+for (i in 1:nrow(AllPromisesRevised)) {
+  for (j in 1:nrow(AllPromises)) {
+    if (stringdist::stringdistmatrix(AllPromisesRevised$Libellé.FR[i],
+                                     AllPromises$`Libellé.(fr)`[j]) <= 30) {
+      matched_rows[i, j] <- TRUE
+    } else {
+      matched_rows[i, j] <- FALSE
+    }
+  }
+}
+# Get indices of matched rows
+matched_indices <- which(matched_rows, arr.ind = TRUE)
+setdiff(1:570, matched_indices[, 1])
+setdiff(1:577, matched_indices[, 2])
+
+# corrélations
+cor(AllPromises$`Importance.pour.la.société.0-10.MOYENNE`,
+    AllPromises$`Large.0-.étroit.10.MOYENNE`)
+cor(AllPromises$`Importance.pour.la.société.0-10.PONDÉRÉE`,
+    AllPromises$`Large.0-.étroit.10.PONDÉRÉ`)
+# la pondération diminue la corrélation
+
+# distribution univariée avant et après pondération, étroitesse et importance
+Plot1 <- ggplot(AllPromises, aes(
+  x = `Importance.pour.la.société.0-10.MOYENNE`)) + geom_bar(width = 0.5) +
+  xlab("Importance de la promesse pour la société (moyenne non pondérée)") +
+  ylab("Nombre de promesses")
+Plot2 <- ggplot(AllPromises, aes(x = `Large.0-.étroit.10.MOYENNE`)) +
+  geom_bar(width = 0.5) +
+  xlab("Degré d'étroitesse de la promesse (moyenne non pondérée)") +
+  ylab("Nombre de promesses")
+Plot3 <- ggplot(AllPromises, aes(
+  x = `Importance.pour.la.société.0-10.PONDÉRÉE`)) + geom_histogram() +
+  xlab("Importance de la promesse pour la société (moyenne pondérée)") +
+  ylab("Nombre de promesses")
+Plot4 <- ggplot(AllPromises, aes(x = `Large.0-.étroit.10.PONDÉRÉ`)) +
+  geom_histogram() +
+  xlab("Degré d'étroitesse de la promesse (moyenne pondérée)") +
+  ylab("Nombre de promesses")
+Plots <- gridExtra::grid.arrange(Plot1, Plot2, Plot3, Plot4)
+#ggsave(paste0("_SharedFolder_polimetre-dev/graphs/",
+#              "ImportanceEtroitessePromessesTrudeau.png"), width = 11,
+#       height = 8.5, Plots)
+
+# suppression de promesses, critère écart-type
+PromisesToDeleteSD <- AllPromises |>
+  dplyr::filter(`Large.0-.étroit.10.PONDÉRÉ` <
+                  (mean(`Large.0-.étroit.10.PONDÉRÉ`) -
+                     sd(`Large.0-.étroit.10.PONDÉRÉ`) * 0.5) |
+                  `Importance.pour.la.société.0-10.PONDÉRÉE` < 
+                  (mean(`Importance.pour.la.société.0-10.PONDÉRÉE`) -
+                     sd(`Importance.pour.la.société.0-10.PONDÉRÉE`) * 0.5))
+length(AllPromises$`Libellé.(fr)`) - length(PromisesToDeleteSD$`Libellé.(fr)`)
+PromisesToDeleteByDomainSD <- data.frame(
+  all = table(AllPromises$Domaine),
+  to_delete = table(PromisesToDeleteSD$Domaine))
+PromisesToDeleteByDomainSD$prop <- PromisesToDeleteByDomainSD$to_delete.Freq /
+  PromisesToDeleteByDomainSD$all.Freq
+
+# suppression de promesses, critère quantile
+PromisesToDeleteQuantile <- AllPromises |>
+  dplyr::filter(`Large.0-.étroit.10.PONDÉRÉ` <
+                  quantile(`Large.0-.étroit.10.PONDÉRÉ`, 0.25) |
+                  `Importance.pour.la.société.0-10.PONDÉRÉE` < 
+                  quantile(`Importance.pour.la.société.0-10.PONDÉRÉE`, 0.25))
+length(AllPromises$`Libellé.(fr)`) -
+  length(PromisesToDeleteQuantile$`Libellé.(fr)`)
+PromisesToDeleteByDomainQuantile <- data.frame(
+  all = table(AllPromises$Domaine),
+  to_delete = table(PromisesToDeleteQuantile$Domaine))
+PromisesToDeleteByDomainQuantile$prop <-
+  PromisesToDeleteByDomainQuantile$to_delete.Freq /
+  PromisesToDeleteByDomainQuantile$all.Freq
+PromisesToDeleteByDomainQuantile$to_delete.Var1 <- NULL
+names(PromisesToDeleteByDomainQuantile) <- c(
+  "Domaine", "Nombre actuel de promesses",
+  "Nombre de promesses à faible importance ou très larges",
+  "% de promesses à faible importance ou très larges")
+#openxlsx::write.xlsx(
+#  PromisesToDeleteByDomainQuantile,
+#  "_SharedFolder_polimetre-dev/data/PromisesToDeleteByDomainQuantile.xlsx")
+#openxlsx::write.xlsx(
+#  PromisesToDeleteQuantile,
+#  "_SharedFolder_polimetre-dev/data/PromisesToDeleteQuantile.xlsx")
