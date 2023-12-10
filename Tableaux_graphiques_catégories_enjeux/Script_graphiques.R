33fa36d76bb4b37014287c52fdde5fbc218a9847
Benjamin Carignan <benjamin.carignan.1@gmail.com>
2023-12-10 15:25:11 -0500

    paufinage des graphs

--- Tableaux_graphiques_catégories_enjeux/Script_graphiques.R
+++ Tableaux_graphiques_catégories_enjeux/Script_graphiques.R
@@ -1,7 +1,11 @@
-## loader tidyverse
+##
+### Charger les packages
+##
 library(tidyverse)
 
-## Importation du csv du 2eme mandat
+##
+### Importer les csv des deux mandats
+##
 dfmandat2 <- read.csv("mandat2_df.csv", header = TRUE,
                       colClasses = c(Catégories = "character",
                                      Réalisée = "numeric",
@@ -10,7 +14,6 @@ dfmandat2 <- read.csv("mandat2_df.csv", header = TRUE,
                                      En.suspens = "numeric",
                                      Rompue = "numeric"))
 
-## Importation du csv du 3eme mandat
 dfmandat3 <- read.csv("mandat3_df.csv", header = TRUE,
                       colClasses = c(Catégories = "character",
                                      Réalisée = "numeric",
@@ -18,10 +21,31 @@ dfmandat3 <- read.csv("mandat3_df.csv", header = TRUE,
                                      En.voie.de.réalisation = "numeric",
                                      En.suspens = "numeric",
                                      Rompue = "numeric"))
+##
+### Renommer les colonnes
+##
+colnames(dfmandat3) <- c("Catégories", "Réalisée", "Partiellement réalisée",
+                         "En voie de réalisation", "En suspens", "Rompue")
+colnames(dfmandat2) <- c("Catégories", "Réalisée", "Partiellement réalisée",
+                         "En voie de réalisation", "En suspens", "Rompue")
 
-## transformation des dataframes avec gather pour les rendres longs
+##
+### Transformer les dataframes avec gather pour les rendres longs
+##
 dfmandat2 <- dfmandat2 %>% gather(key = "Status", value = "Value", -Catégories)
 dfmandat3 <- dfmandat3 %>% gather(key = "Status", value = "Value", -Catégories)
+
+##
+### Transformer la variable Status en Facteur et ordonner comme souhaité
+##
+dfmandat2$Status <- factor(dfmandat2$Status, 
+                           levels = c("Réalisée", "Partiellement réalisée",
+                                      "En voie de réalisation", "En suspens", 
+                                      "Rompue"))
+dfmandat3$Status <- factor(dfmandat3$Status, 
+                           levels = c("Réalisée", "Partiellement réalisée",
+                                      "En voie de réalisation", "En suspens", 
+                                      "Rompue"))
 
 ## Création de la palette de couleur 
 
@@ -30,48 +54,52 @@ verdict_palette <- c("#CCCCCC", "#999999", "#666666", "#333333", "black")
 ## graphique du 2eme mandat
 
 
-graphmandat2 <- ggplot(dfmandat2, aes(x = Status , y = Value ,fill = Status)) +
+graphmandat2 <- ggplot(dfmandat2, 
+                       aes(x = Value , y = Catégories ,fill = Status)) +
   geom_bar(stat = "identity")  +
+  geom_text(data = subset(dfmandat2, Value != 0), 
+            aes(label = Value), position = position_stack(vjust = 0.5),
+            size = 3, color = "white") +
   scale_fill_manual(values = verdict_palette) +
   labs(
-    title = "Verdicts du troisième mandat selon les catégories d'enjeux",
-    x = NULL,
-    y = NULL
-  ) +
+    title = "Verdicts du second mandat selon les catégories d'enjeux",
+    x = "Nombre de promesses",
+    y = "Catégories d'enjeux",
+    fill = "Legend Title") +
   clessnverse::theme_clean_light(base_size = 15) +
   theme(
     plot.title = element_text(size = 15, hjust = 0.5), 
     axis.title.x = element_text(size = 12, hjust = 0.5),
     axis.title.y = element_text(size = 12, hjust = 0.5),
     axis.text = element_text(size = 8),            
-    legend.position = "none",
-    axis.text.x = element_text(angle = 80, hjust=1)         
-  ) + facet_wrap(~ Catégories)
+    legend.position = "right")
 
-ggsave("mandat2_plot.png", plot = graphmandat3, width = 12, height = 6
+ggsave("mandat2_plot.png", plot = graphmandat2, width = 12, height = 6
 )
 print(graphmandat2)
 
 
 ## graphique du 3eme mandat
 
-graphmandat3 <- ggplot(dfmandat3, aes(x = Status , y = Value ,fill = Status)) +
+graphmandat3 <- ggplot(dfmandat3, 
+                       aes(x = Value , y = Catégories ,fill = Status)) +
   geom_bar(stat = "identity")  +
+  geom_text(data = subset(dfmandat3, Value != 0), 
+            aes(label = Value), position = position_stack(vjust = 0.5),
+            size = 3, color = "white") +
   scale_fill_manual(values = verdict_palette) +
   labs(
     title = "Verdicts du troisième mandat selon les catégories d'enjeux",
-    x = NULL,
-    y = NULL
-  ) +
+    x = "Nombre de promesses",
+    y = "Catégories d'enjeux",
+    fill = "Legend Title") +
   clessnverse::theme_clean_light(base_size = 15) +
   theme(
     plot.title = element_text(size = 15, hjust = 0.5), 
     axis.title.x = element_text(size = 12, hjust = 0.5),
     axis.title.y = element_text(size = 12, hjust = 0.5),
     axis.text = element_text(size = 8),            
-    legend.position = "none",
-    axis.text.x = element_text(angle = 80, hjust=1)         
-  ) + facet_wrap(~ Catégories)
+    legend.position = "right")
 
 ggsave("mandat3_plot.png", plot = graphmandat3, width = 12, height = 6
        )
