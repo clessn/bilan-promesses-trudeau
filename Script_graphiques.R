a8a707f36978a4f30f91d8cd286647835d26abd6
Benjamin Carignan <benjamin.carignan.1@gmail.com>
2023-12-13 10:42:31 -0500

    changement du nom axe des X

--- Script_graphiques.R
+++ Script_graphiques.R
@@ -9,7 +9,7 @@ library(dplyr)
 
 dfexcelchap1 <- read.xlsx("_SharedFolder_livre_promesses-trudeau/Chapitre 1/PolimètreTrudeau-Chapitre1.xlsx", 3)
 dftest2 <- dfexcelchap1 |>
-  group_by(Catégorie, Mandat, Verdict,) |>
+  group_by(`Catégorie./.Category`, Mandat, Verdict,) |>
   summarise(Value = n())
 colnames(dftest2) <- c("Catégories", "Mandat","Status", "Value" )
 
@@ -45,7 +45,7 @@ graphmandat2 <- dftest2_percent |>
   scale_fill_manual(values = verdict_palette2) +
   labs(
     title = "Verdicts du second mandat selon les catégories d'enjeux",
-    x = "Nombre de promesses",
+    x = "Pourcentage de promesses",
     y = "Catégories d'enjeux",
     fill = "Legend Title") +
   clessnverse::theme_clean_light(base_size = 15) +
@@ -81,7 +81,7 @@ graphmandat3 <- dftest3_percent |>
   scale_fill_manual(values = verdict_palette1) +
   labs(
     title = "Verdicts du troisième mandat selon les catégories d'enjeux",
-    x = "Nombre de promesses",
+    x = "Pourcentage de promesses",
     y = "Catégories d'enjeux",
     fill = "Legend Title") +
   clessnverse::theme_clean_light(base_size = 15) +
@@ -95,4 +95,115 @@ graphmandat3 <- dftest3_percent |>
 ggsave("_SharedFolder_livre_promesses-trudeau/Chapitre 1/graphs/mandat3_plot.png", plot = graphmandat3, width = 12, height = 6
 )
 print(graphmandat3)
+
+## ANGLAIS
+
+dfexcelchap1 <- read.xlsx("_SharedFolder_livre_promesses-trudeau/Chapitre 1/PolimètreTrudeau-Chapitre1.xlsx", 3)
+dfcategoverdiENG <- dfexcelchap1 |>
+  group_by(`Catégorie./.Category`, Mandat, Verdict,) |>
+  summarise(Value = n())
+colnames(dfcategoverdiENG) <- c("Categories", "Term","Status", "Value" )
+
+dfcategoverdiENG <- dfcategoverdiENG %>%
+  mutate(Categories = case_when(
+    Categories == "Affaires internationales et défense" ~ "International Affairs and Defense",
+    Categories == "Arts et culture" ~ "Arts and Culture",
+    Categories == "Économie et employabilité" ~ "Economy and Employability",
+    Categories == "Éducation et recherche" ~ "Education and Research",
+    Categories == "Environnement" ~ "Environment",
+    Categories == "Familles" ~ "Families",
+    Categories == "Gouvernement et démocratie" ~ "Government and Democracy",
+    Categories == "Identité et nationalisme" ~ "Identity and Nationalism",
+    Categories == "Loi et ordre" ~ "Law and Order",
+    Categories == "Minorités" ~ "Minorities",
+    Categories == "Régions et agriculture" ~ "Regions and Agriculture",
+    Categories == "Santé et services sociaux" ~ "Health and Social Service",
+    TRUE ~ Categories)) 
+
+dfcategoverdiENG <- dfcategoverdiENG %>%
+  mutate(Status = case_when(
+    Status == "Partiellement réalisée" ~ "Partially kept",
+    Status == "Rompue" ~ "Broken",
+    Status == "Réalisée" ~ "Kept",
+    Status == "En suspens" ~ "Not yet rated",
+    Status == "En voie de réalisation" ~ "In progress",
+    TRUE ~ Status)) 
+##
+### Transformer la variable Status en Facteur et ordonner comme souhaité
+##
+dfcategoverdiENG$Status <- factor(dfcategoverdiENG$Status, 
+                         levels = c("Kept", "Partially kept",
+                                    "In progress", "Not yet rated", 
+                                    "Broken"))
+
+## Création de la palette de couleur 
+
+verdict_palette1 <- c("#CCCCCC", "#999999", "#666666", "#333333", "black")
+verdict_palette2 <- c("#999999", "#666666", "black")
+
+## graphique du 2eme mandat
+
+dfcategoverdiENG_percent <- dfcategoverdiENG %>%
+  filter(Term == 2) %>%
+  group_by(Categories) %>%
+  mutate(Percentage = Value / sum(Value) * 100, 
+         Value2 = sum(Value),
+         Categories2 = paste0(Categories, " N = ", Value2))
+
+graphmandat2ENG <- dfcategoverdiENG_percent |>
+  ggplot(aes(x = Percentage , y = Categories2 ,fill = Status)) +
+  geom_bar(stat = "identity")  +
+  geom_text(data = dfcategoverdiENG_percent, 
+            aes(label = round(Percentage)), position = position_stack(vjust = 0.5),
+            size = 3, color = "white") +
+  scale_fill_manual(values = verdict_palette2) +
+  labs(
+    title = "Second-term verdicts by issue category",
+    x = "Percent of promises",
+    y = "Issue categories",
+    fill = "Legend Title") +
+  clessnverse::theme_clean_light(base_size = 15) +
+  theme(
+    plot.title = element_text(size = 15, hjust = 0.5), 
+    axis.title.x = element_text(size = 12, hjust = 0.5),
+    axis.title.y = element_text(size = 12, hjust = 0.5),
+    axis.text = element_text(size = 8),            
+    legend.position = "right")
+
+ggsave("_SharedFolder_livre_promesses-trudeau/Chapitre 1/graphs/mandat2ENG_plot.png", plot = graphmandat2ENG, width = 12, height = 6
+)
+print(graphmandat2ENG)
+
+
+## graphique du 3eme mandat
+dfcategoverdiENG_percent <- dfcategoverdiENG %>%
+  filter(Term == 3) %>%
+  group_by(Categories) %>%
+  mutate(Percentage = Value / sum(Value) * 100, 
+         Value2 = sum(Value),
+         Categories2 = paste0(Categories, " N = ", Value2))
+
+graphmandat3ENG <- dfcategoverdiENG_percent |>
+  ggplot(aes(x = Percentage , y = Categories2 ,fill = Status)) +
+  geom_bar(stat = "identity")  +
+  geom_text(data = dfcategoverdiENG_percent, 
+            aes(label = round(Percentage)), position = position_stack(vjust = 0.5),
+            size = 3, color = "white") +
+  scale_fill_manual(values = verdict_palette1) +
+  labs(
+    title = "Third-term verdicts by issue category",
+    x = "Percent of promises",
+    y = "Issue categories",
+    fill = "Legend Title") +
+  clessnverse::theme_clean_light(base_size = 15) +
+  theme(
+    plot.title = element_text(size = 15, hjust = 0.5), 
+    axis.title.x = element_text(size = 12, hjust = 0.5),
+    axis.title.y = element_text(size = 12, hjust = 0.5),
+    axis.text = element_text(size = 8),            
+    legend.position = "right")
+
+ggsave("_SharedFolder_livre_promesses-trudeau/Chapitre 1/graphs/mandat3ENG_plot.png", plot = graphmandat3ENG, width = 12, height = 6
+)
+print(graphmandat3ENG)
 
