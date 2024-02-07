61e0054ac9f65ba6fdf4184785c9deb922256d33
hubcad25 <hubertcadieux@gmail.com>
2024-02-06 22:29:54 -0500

    derniers modifs graph

--- graph_intime_trudeauVSharper.R
+++ graph_intime_trudeauVSharper.R
@@ -5,6 +5,13 @@ library(dplyr)
 # Data --------------------------------------------------------------------
 Data <- readRDS("_SharedFolder_livre_promesses-trudeau/Chapitre 1/data/output_graph_trudeauharper.rds")
 
+labels <- c("harper1" = "<span style='color:grey70'> Harper I (2006-2008)* </span>",
+            "trudeau1" = "Trudeau I (2015-2019)**",
+            "harper2" = "<span style='color:grey70'> Harper II (2008-2011)* </span>",
+            "trudeau2" = "Trudeau II (2019-2021)*",
+            "harper3" = "<span style='color:grey70'> Harper III (2011-2015)** </span>",
+            "trudeau3" = "Trudeau III (2021-...)*")
+
 graph <- Data %>% 
   group_by(mandate_id, date_limit) %>% 
   summarise(sum_realisee = sum(realisee),
@@ -13,48 +20,58 @@ graph <- Data %>%
          prop = (sum_realisee / n) * 100,
          pm = substr(mandate_id, 1, nchar(mandate_id) - 1),
          mandate = substr(mandate_id, nchar(mandate_id), nchar(mandate_id)),
-         mandate_id = factor(mandate_id, levels = c("harper1", "trudeau1", "harper2", "trudeau2", "harper3", "trudeau3")))
-
+         mandate_id = factor(mandate_id, levels = names(labels)))
 
 ## Points à ajouter
 # 9 mars 2020 pour COVID
-## Guerre en Ukraine
+# 15 septembre 2008 pour crise économique
 DatesImportantes <- data.frame(
-  date_limit = as.Date(c("2020-01-08",
-                         "2020-01-20",
-                         "2020-03-11",
-                         "2022-02-24")),
-  event = c("Avion civil ukrainien abattu en Iran",
-            "Début du blocus autochtone anti-gazoduc",
-            "Pandémie mondiale déclarée par l'OMS",
-            "Début de l'invasion de l'Ukraine par la Russie")
+  date_limit = as.Date(c("2020-03-09")),
+  event = c("Pandémie mondiale\ndéclarée par l'OMS")
 ) %>% 
   mutate(label = paste0(format(as.Date(date_limit), "%d %B %Y"), "\n", event)) %>% 
   left_join(graph, ., by = "date_limit") %>% 
-  drop_na(event, label)
-
-
-labels <- c("harper1" = "<span style='color:grey70'> Harper I (2006-2008)* </span>",
-            "harper2" = "<span style='color:grey70'> Harper II (2008-2011)* </span>",
-            "harper3" = "<span style='color:grey70'> Harper III (2011-2015)** </span>",
-            "trudeau1" = "Trudeau I (2015-2019)**",
-            "trudeau2" = "Trudeau II (2019-2021)*",
-            "trudeau3" = "Trudeau III (2021-...)*")
+  tidyr::drop_na(event, label) %>% 
+  mutate(mandate_id = factor(mandate_id, levels = names(labels)))
 
 ggplot(graph, aes(x = day_in_mandate, y = prop,
                   group = mandate_id, linetype = mandate_id,
                   color = mandate_id)) +
+  geom_text(data = DatesImportantes,
+            angle = 90, hjust = 1,
+            y = 95, vjust = 0.5,
+            aes(label = label, x = day_in_mandate - 40),
+            size = 3, show.legend = FALSE) +
+  geom_curve(data = DatesImportantes,
+             aes(x = day_in_mandate - 40, xend = day_in_mandate - 2,
+                 yend = prop), curvature = 0.2,
+             y = 65, linetype = "solid", linewidth = 0.15,
+             show.legend = FALSE) +
   geom_line(linewidth = 0.5) +
+  geom_point(data = DatesImportantes, size = 2,
+             alpha = 1, show.legend = FALSE,
+             aes(y = prop - 0.5),
+             shape = 21,
+             fill = "grey90",
+             stroke = 0.5) +
+  geom_point(data = DatesImportantes,
+             aes(y = prop - 0.5),
+             alpha = 1, show.legend = FALSE,
+             shape = 19,
+             size = 0.25,
+             color = "black") +
   clessnverse::theme_clean_light() +
   scale_color_manual(values = c("harper1" = "grey70", "harper2" = "grey70", "harper3" = "grey70",
                                 "trudeau1" = "black", "trudeau2" = "black", "trudeau3" = "black"),
-                     labels = labels) +
+                     labels = labels, limits = names(labels)) +
   scale_linetype_manual(values = c("harper1" = "dotted", "harper2" = "dashed", "harper3" = "solid",
                                    "trudeau1" = "dotted", "trudeau2" = "dashed", "trudeau3" = "solid"),
-                        labels = labels) +
+                        labels = labels, limits = names(labels)) +
   ylab("Proportion des promesses réalisées, partiellement\nréalisées ou en voie de réalisation à ce jour (%)") +
   xlab("Jour dans le mandat") +
-  labs(caption = "* = gouvernement minoritaire\n** = gouvernement majoritaire") +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100),
+                     limits = c(0, 100)) +
+  labs(caption = "* = gouvernement minoritaire\n** = gouvernement majoritaire\nLa crise financière mondiale de 2008 a commencé entre les deux premiers mandats de Stephen Harper.") +
   theme(axis.title.x = element_text(hjust = 0.5),
         axis.title.y = element_text(hjust = 0.5),
         legend.text=ggtext::element_markdown(size=12))
